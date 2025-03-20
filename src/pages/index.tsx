e4510d001d187c8fc91f319330ef2b7a1864166b
Roberto Ruiz <R.Ruiz@ext.aurubis.com>
2025-03-20 17:48:25 +0100

    feat: add footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { themeChange } from "theme-change";
 import Module from "@/components/Section";
 import plan from "@/data/plan";
 import ThemeSelector from "@/components/ThemeSelector";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   useEffect(() => {
@@ -13,14 +14,15 @@ export default function Home() {
   return (
     <div>
       <ThemeSelector />
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
         <h1 className="text-4xl font-bold">{plan.title}</h1>
         <div className="grid gap-8">
           {plan.sections.map((section) => (
             <Module key={section.title} section={section} />
           ))}
         </div>
-      </div>
+      </main>
+      <Footer />
     </div>
   );
 }
