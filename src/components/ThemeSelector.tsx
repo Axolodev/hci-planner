dc0be766cbe980f3cae1ae39e18eb59c7ebff7d4
Roberto Ruiz <R.Ruiz@ext.aurubis.com>
2025-03-20 17:09:35 +0100

    feat: add all daisyUI themes to changer

--- src/components/ThemeSelector.tsx
+++ src/components/ThemeSelector.tsx
@@ -1,13 +1,40 @@
+// Lists all themes available in the daisyUI library
+const daisyUiThemes = [
+  "default",
+  "dark",
+  "cupcake",
+  "bumblebee",
+  "emerald",
+  "corporate",
+  "synthwave",
+  "retro",
+  "cyberpunk",
+  "valentine",
+  "halloween",
+  "garden",
+  "forest",
+  "aqua",
+  "lofi",
+  "pastel",
+  "fantasy",
+  "wireframe",
+  "black",
+  "luxury",
+  "dracula",
+];
+
 export default function ThemeSelector() {
   return (
     <div>
       <select data-choose-theme className="select">
-        <option value="" disabled>
+        <option value="" disabled selected>
           Choose a theme
         </option>
-        <option value="">Default</option>
-        <option value="dark">Dark</option>
-        <option value="cupcake">Cupcake</option>
+        {daisyUiThemes.map((theme) => (
+          <option key={theme} value={theme}>
+            {theme.charAt(0).toUpperCase() + theme.slice(1)}
+          </option>
+        ))}
       </select>
     </div>
   );
