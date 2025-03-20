2181b7d397ed4de4b672ef188407ca7cd072d1c8
Roberto Ruiz <R.Ruiz@ext.aurubis.com>
2025-03-20 17:37:24 +0100

    feat: improve theme changer

--- src/components/ThemeSelector.tsx
+++ src/components/ThemeSelector.tsx
@@ -25,17 +25,40 @@ const daisyUiThemes = [
 
 export default function ThemeSelector() {
   return (
-    <div>
-      <select data-choose-theme className="select">
-        <option value="" disabled selected>
-          Choose a theme
-        </option>
+    <div className="dropdown mb-72 absolute">
+      <div tabIndex={0} role="button" className="btn m-1">
+        Choose a Theme
+        <svg
+          width="12px"
+          height="12px"
+          className="inline-block h-2 w-2 fill-current opacity-60"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 2048 2048"
+        >
+          <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-2xl"
+      >
         {daisyUiThemes.map((theme) => (
-          <option key={theme} value={theme}>
-            {theme.charAt(0).toUpperCase() + theme.slice(1)}
-          </option>
+          <li
+            key={theme}
+            data-theme={theme}
+            data-set-theme={theme}
+            className="not-first:mt-1"
+          >
+            <input
+              type="radio"
+              name="theme-dropdown"
+              className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+              aria-label={theme.charAt(0).toUpperCase() + theme.slice(1)}
+              value={theme}
+            />
+          </li>
         ))}
-      </select>
+      </ul>
     </div>
   );
 }
