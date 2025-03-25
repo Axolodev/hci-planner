626e5a03a8462aad6b9e909dfe2902aee976f5ef
Roberto Ruiz <R.Ruiz@ext.aurubis.com>
2025-03-25 13:41:09 +0100

    feat: add favicon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,19 @@
+import { FaGithub } from "react-icons/fa";
+
 export default function Footer() {
   return (
-    <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
-      <aside>
+    <footer className="footer footer-horizontal bg-base-300 text-base-content p-4">
+      <nav className="grid-flow-col text-2xl gap-4 sm:place-self-center sm:justify-self-start">
+        <a
+          href="https://github.com/Axolodev/hci-planner"
+          title="Go to project's GitHub repository"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <FaGithub />
+        </a>
+      </nav>
+      <aside className="grid-flow-col gap-4 place-self-center justify-self-end">
         <div>
           Created with ❤️ by{" "}
           <a
